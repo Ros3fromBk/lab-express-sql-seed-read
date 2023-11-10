80aea9ff55ea7c613d29b35691b76430b2c76469
Rose <i.a.rose.bk@gmail.com>
2023-11-10 09:30:27 -0500

    adding app.js

--- back-end/app.js
+++ back-end/app.js
@@ -19,6 +19,12 @@ app.get('/', (req, res) => {
     res.send("WELCOMEEE");
 });
 
+// 404 Route Handler
+app.use((req, res, next) => {
+  res.status(404).send('Sorry, the page you are looking for does not exist.');
+});
+
+
 
 
  module.exports = app;
